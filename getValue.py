429c2e38996c2f0e31f2613a79d8a555e8e38769
阿瑜 <goish2468@gmail.com>
2018-07-08 21:31:42 +0800

    Add files via upload

---
+++ getValue.py
@@ -0,0 +1,49 @@
+import xml.etree.ElementTree as ET
+import xlwt
+
+# create table
+table = xlwt.Workbook() 
+# create sheet
+sheet = table.add_sheet('sheet1')
+sheet_2 = table.add_sheet('sheet2')
+
+# read from static.xml
+tree = ET.parse('static.xml')
+root = tree.getroot()
+# read from dynamic.xml
+tree2 = ET.parse('dynamic.xml')
+root2 = tree2.getroot()
+
+# using Element.iter() to get routeid & roadsection
+# write into spped.xls
+counter = 0
+for info in root.iter('Info'):
+    #print (info.attrib['routeid'], info.attrib['roadsection'])
+    #print('/')
+    sheet.write(counter,0,info.attrib['routeid'])
+    sheet.write(counter,1,info.attrib['roadsection'])
+    for info2 in root2.iter('Info'):
+        if info2.attrib['routeid'] == info.attrib['routeid']:
+            sheet.write(counter,2,info2.attrib['value'])
+            sheet.write(counter,3,info2.attrib['datacollecttime'])
+            break
+    counter = counter + 1 
+
+# read from dynamic.xml
+#tree2 = ET.parse('dynamic.xml')
+#root2 = tree2.getroot()
+
+# get routeid & value & datacollecttme 
+#counter2 = 0
+#for info in root2.iter('Info'):
+#    sheet_2.write(counter2,0,info.attrib['routeid'])
+#    sheet_2.write(counter2,1,info.attrib['value'])
+#    sheet_2.write(counter2,2,info.attrib['datacollecttime'])
+#    counter2 = counter2 + 1
+
+
+# save table
+table.save('getValue.xls')
+
+
+
